4fd54b3b90c4e166fda8077590353b530b675754
Aoishik Khan <73829596+aoishik@users.noreply.github.com>
2026-07-09 22:48:25 +0530

    DEBUG_CHANGE

--- nephthys/database/raw_migration.py
+++ nephthys/database/raw_migration.py
@@ -27,18 +27,28 @@ def raw_migration(
     backwards: str | None = None,
 ):
     manager = MigrationManager(
-        migration_id=migration_id, app_name=app_name, description=description
+        migration_id=migration_id,
+        app_name=app_name,
+        description=description,
     )
 
     async def run():
-        await RawTable.raw(postgresql_code_block(forwards))
+        sql = postgresql_code_block(forwards)
+        print("=" * 80)
+        print(sql)
+        print("=" * 80)
+        await RawTable.raw(sql)
 
     manager.add_raw(run)
 
     if backwards:
 
         async def run_backwards():
-            await RawTable.raw(postgresql_code_block(backwards))
+            sql = postgresql_code_block(backwards)
+            print("=" * 80)
+            print(sql)
+            print("=" * 80)
+            await RawTable.raw(sql)
 
         manager.add_raw_backwards(run_backwards)
 
